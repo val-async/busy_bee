fca0481abf9b3f93be08531956ac661116a7ec8f
valo <val.async@gmail.com>
2026-04-09 20:11:27 +0100

    settings.py

--- busy_bee_project/busy_bee_project/settings.py
+++ busy_bee_project/busy_bee_project/settings.py
@@ -83,23 +83,24 @@ WSGI_APPLICATION = 'busy_bee_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if not DEBUG:
-    # Use PostgreSQL on Render
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
-else:
-    # Use SQLite locally
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if not DEBUG:
+#     # Use PostgreSQL on Render
 
+# else:
+#     # Use SQLite locally
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
