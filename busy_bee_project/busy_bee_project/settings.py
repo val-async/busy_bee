8b5ae82b2461c315c6a6b3e06c931edb03a40535
valo <val.async@gmail.com>
2026-04-09 18:59:58 +0100

    update settings

--- busy_bee_project/busy_bee_project/settings.py
+++ busy_bee_project/busy_bee_project/settings.py
@@ -146,7 +146,11 @@ if not DEBUG:
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
